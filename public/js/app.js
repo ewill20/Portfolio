b8f2257ef3e089914fb6bab87046ec3ce30a7f62
ewill20 <ewill2020@gmail.com>
2019-07-13 14:00:32 -0500

    setting up app.js for click functions and moving panels

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	console.log("The document is ready and raring to go!");
+});
+
+// limited click listeners for homepage links //
+$('#WebDevSSPanel').on('click', function() {
+	window.location = '/webPort';
+})
